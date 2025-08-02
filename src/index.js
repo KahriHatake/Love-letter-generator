229bee040ab87196d7cf795a7c7655eb1a8a7403
Kari Aguilar <aguilar.kari@icloud.com>
2025-08-02 00:54:13 -0700

    revised the prompt and the delay of the typewriter plugin

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function displayPoem(response) {
   new Typewriter(poemElement, {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 20,
     cursor: "",
   });
 }
@@ -18,7 +18,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4b08f845751300d9odtfbffbbf0216ac";
   let prompt = `User instruction: Generate a sonnet about ${instructionsInput.value}`;
-  let context = `let context = "You are a poem-writing expert who generates heartfelt short poems. Return sing-song an eight line poem using simple <br> tags for new lines. Do not wrap the output in <html> or <body> tags. Only return the poem.`;
+  let context = `let context = "You are a poem-writing expert who generates heartfelt short poems. Return a 10 line poem that rhymes using simple <br> tags for new lines. Do not wrap the output in <html> or <body> tags. Only return the poem.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
   console.log("Generating poem...");
